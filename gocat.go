627d0a24ae4560c8bce73a5f8aa5d82929bf11e2
Kare Nuorteva <kare.nuorteva@me.com>
2013-08-01 14:26:06 +0300

    Add some basic functionality
    Signed-off-by: Kare Nuorteva <kare.nuorteva@me.com>

--- gocat.go
+++ gocat.go
@@ -1,12 +1,36 @@
 package main
 
 import (
-    "io"
-    "os"
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
 func main() {
-    if 1 == len(os.Args) {
-        io.Copy(os.Stdout, os.Stdin)
-    }
+	if 2 == len(os.Args) && os.Args[1] == "-h" {
+		Help()
+	}
+	if 1 == len(os.Args) {
+		io.Copy(os.Stdout, os.Stdin)
+	}
+	if 2 == len(os.Args) {
+		var (
+			file *os.File
+			err  error
+		)
+		if file, err = os.Open(os.Args[1]); err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+		defer file.Close()
+		reader := bufio.NewReader(file)
+		io.Copy(os.Stdout, reader)
+	}
+}
+
+func Help() {
+	fmt.Println("HELP")
+	os.Exit(0)
 }
