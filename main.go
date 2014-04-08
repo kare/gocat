f6f4d6d9a6c2d6ac5b2fa2276d99308d9dceb988
Kare Nuorteva <kare.nuorteva@me.com>
2014-04-08 03:57:00 +0300

    Extract cat()
    Signed-off-by: Kare Nuorteva <kare.nuorteva@me.com>

--- main.go
+++ main.go
@@ -8,13 +8,17 @@ import (
 	"os"
 )
 
+func cat(dst io.Writer, src io.Reader) (written int64, err error) {
+	return io.Copy(dst, src)
+}
+
 func main() {
 	if 2 == len(os.Args) && os.Args[1] == "-h" {
 		fmt.Fprintf(os.Stderr, "usage: %s [file]\n", os.Args[0])
 		os.Exit(0)
 	}
 	if 1 == len(os.Args) {
-		if _, err := io.Copy(os.Stdout, os.Stdin); err != nil {
+		if _, err := cat(os.Stdout, os.Stdin); err != nil {
 			panic(err)
 		}
 	}
@@ -28,7 +32,7 @@ func main() {
 			os.Exit(1)
 		}
 		defer file.Close()
-		if _, err := io.Copy(os.Stdout, bufio.NewReader(file)); err != nil {
+		if _, err := cat(os.Stdout, bufio.NewReader(file)); err != nil {
 			panic(err)
 		}
 	}
